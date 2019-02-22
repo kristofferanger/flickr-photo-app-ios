0a05833f1e64016e3d1dd27e236dc0519fe5670d
Kristoffer Anger <kristoffer.anger@gmail.com>
2019-02-22 16:55:42 +0100

    Initial Commit

---
+++ FlickrFotoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrFotoApp
+//
+//  Created by Kristoffer Anger on 2019-02-22.
+//  Copyright © 2019 kriang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrFotoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrFotoApp
+//
+//  Created by Kristoffer Anger on 2019-02-22.
+//  Copyright © 2019 kriang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
